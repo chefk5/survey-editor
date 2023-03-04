ac00c96d008b0a75c642b0600040803a3b6e2462
Khaledc <kncharkie@gmail.com>
2023-03-04 22:11:55 +0200

    Add initial project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Avatar from 'components/Avatar'
+
+function App() {
+  return (
+    <div className="relative overflow-hidden bg-white">
+      <p>heeloo</p>
+    </div>
+  )
+}
+
+export default App
